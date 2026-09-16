ee9bd821907500ff5d476f17bf7182f12b1a39f6
jasonlou2005-cpu <jaslou2005@gmail.com>
2026-09-15 23:59:55 -0700

    Add files via upload

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import SiteNavigation from "@/components/SiteNavigation";
 import HeroVideo from "@/components/HeroVideo";
 import DividerSection from "@/components/DividerSection";
 import ThreeExperience from "@/components/ThreeExperience";
+import SystemsSection from "@/components/SystemsSection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <HeroVideo />
         <DividerSection />
         <ThreeExperience />
+        <SystemsSection />
         <Footer />
       </main>
     </>
